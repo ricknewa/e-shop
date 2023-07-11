293b8889a26210553b0d7b39ebd70881f42b444f
ricknewa <ricknewa01@gmail.com>
2023-07-11 16:03:06 +0545

    3

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    
+                    @foreach ($products as $product)
+                    <td>
+                        <a href="{{ route('product.show', $product->id) }}">
+                            <div class="flex justify-between w-full hover:bg-slate-700 rounded-xl">
+                                <div class=" basis-2/5">
+                                    <p class=" p-2">
+                                        {{ Str::words($product->title, 1, '') }}
+                                    </p>
+                                </div>
+                                <div class=" basis-2/5">
+                                    <p class=" p-2"> {{ $product->created_at->diffForHumans() }} </p>
+                                </div>
+                                <div class=" basis-1/5 ">
+                                    <p class=" p-2 ">{{ $product->price }} </p>
+                                </div>
+                            </div>
+                        </a>
+                    </td>
+                @endforeach
                 </div>
             </div>
         </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,8 @@
- <nav x-data="{ open: false }" class=" bg-black border-b-4 border-lime-500 justify-stretch ">
-     <!-- Primary Navigation Menu -->
-     {{-- first navigation  --}}
+ <nav x-data="{ open: false }" class="  fixed right-0 left-0 top-0">
+    <div class="bg-black border-b-4 text-justify border-lime-500 justify-stretch">
+
+        <!-- Primary Navigation Menu -->
+        {{-- first navigation  --}}
      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
          <div class="flex justify-between h-16">
              <div class="flex">
@@ -15,12 +17,14 @@
              <!-- Settings Dropdown -->
              <div class="hidden sm:flex sm:items-center sm:ml-6">
                  @auth
+                 <div class="overflow-hidden h-12 w-12">
                      @if (auth()->user()->avatar)
                          <a href="{{ route('profile.edit') }}">
-                             <img class="rounded-md h-12 mr-4 overflow-hidden" {{ $avt = auth()->user()->avatar }}
-                                 src="{{ "/storage/$avt" }}">
+                             <img class="rounded-full aspect-square object-cover h-10"
+                                 {{ $avt = auth()->user()->avatar }} src="{{ "/storage/$avt" }}">
                          </a>
                      @endif
+                 </div>
                      <x-dropdown align="right" width="48">
                          <x-slot name="trigger">
                              <button
@@ -55,7 +59,7 @@
                              </form>
                          </x-slot>
                      </x-dropdown>
-                     {{-- secon navigation --}}
+                     {{-- second navigation --}}
                  @endauth
 
              </div>
@@ -75,94 +79,59 @@
              </div>
          </div>
      </div>
-
+     
      <!-- Responsive Navigation Menu -->
-     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-         <div class="pt-2 pb-3 space-y-1">
-             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                 {{ __('Dashboard') }}
-             </x-responsive-nav-link>
-         </div>
-
-         <!-- Responsive Settings Options -->
-         <div class="pt-4 pb-1 border-t border-gray-200 ">
-             <div class="px-4">
-                 <div class="font-medium text-base text-gray-800 ">{{ Auth::user()->name }}</div>
-                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-             </div>
-
-             <div class="mt-3 space-y-1">
-                 <x-responsive-nav-link :href="route('profile.edit')">
-                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                         <path fill-rule="evenodd"
-                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
-                             clip-rule="evenodd" />
-                     </svg>
-
-                     {{ __('Profile') }}
-                 </x-responsive-nav-link>
-
-                 <!-- Authentication -->
-                 <form method="POST" action="{{ route('logout') }}">
-                     @csrf
-
-                     <x-responsive-nav-link :href="route('logout')"
-                         onclick="event.preventDefault(); this.closest('form').submit();">
-                         {{ __('Log Out') }}
-                     </x-responsive-nav-link>
-                 </form>
-             </div>
-         </div>
-     </div>
- </nav>
- <nav x-data="{ open: false }" class=" bg-black  flex  justify-center bg-opacity-70">
-     <div class="hidden space-x-8 sm:-mp-px sm:pl-10 sm:flex">
+     
+    </div>
+    <div class="bg-stone-900 text-justify  flex bg justify-evenly">
+        <div class=" space-x-8 sm:-mp-px sm:pl-10 sm:flex">
          {{-- home --}}
          <x-nav-link :href="route('dashboard')">
              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                  <path fill-rule="evenodd"
-                     d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
-                     clip-rule="evenodd" />
-             </svg>
-
-             {{ __('Home') }}
-         </x-nav-link>
-     </div>
-     <!-- product -->
-     <div class="hidden space-x-8 sm:-py-px sm:pl-10 sm:flex">
-         <x-nav-link :href="route('product.index')">
-             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                 <path
-                     d="M2 4.5A2.5 2.5 0 014.5 2h11a2.5 2.5 0 010 5h-11A2.5 2.5 0 012 4.5zM2.75 9.083a.75.75 0 000 1.5h14.5a.75.75 0 000-1.5H2.75zM2.75 12.663a.75.75 0 000 1.5h14.5a.75.75 0 000-1.5H2.75zM2.75 16.25a.75.75 0 000 1.5h14.5a.75.75 0 100-1.5H2.75z" />
-             </svg>
-
-             {{ __('Product List') }}
-         </x-nav-link>
-     </div>
-     <!-- catagory -->
-     <div class="hidden space-x-8 sm:-py-px sm:pl-10 sm:flex">
+                 d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
+                 clip-rule="evenodd" />
+                </svg>
+                
+                {{ __('Home') }}
+            </x-nav-link>
+        </div>
+        <!-- product -->
+        <div class=" space-x-8 sm:-py-px sm:pl-10 sm:flex">
+            <x-nav-link :href="route('product.index')">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                    <path
+                    d="M2 4.5A2.5 2.5 0 014.5 2h11a2.5 2.5 0 010 5h-11A2.5 2.5 0 012 4.5zM2.75 9.083a.75.75 0 000 1.5h14.5a.75.75 0 000-1.5H2.75zM2.75 12.663a.75.75 0 000 1.5h14.5a.75.75 0 000-1.5H2.75zM2.75 16.25a.75.75 0 000 1.5h14.5a.75.75 0 100-1.5H2.75z" />
+                </svg>
+                
+                {{ __('Product List') }}
+            </x-nav-link>
+        </div>
+        <!-- catagory -->
+        <div class=" space-x-8 sm:-py-px sm:pl-10 sm:flex">
+            <x-nav-link :href="route('product.index')">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                    <path fill-rule="evenodd"
+                    d="M2.5 3A1.5 1.5 0 001 4.5v4A1.5 1.5 0 002.5 10h6A1.5 1.5 0 0010 8.5v-4A1.5 1.5 0 008.5 3h-6zm11 2A1.5 1.5 0 0012 6.5v7a1.5 1.5 0 001.5 1.5h4a1.5 1.5 0 001.5-1.5v-7A1.5 1.5 0 0017.5 5h-4zm-10 7A1.5 1.5 0 002 13.5v2A1.5 1.5 0 003.5 17h6a1.5 1.5 0 001.5-1.5v-2A1.5 1.5 0 009.5 12h-6z"
+                    clip-rule="evenodd" />
+                </svg>
+                {{ __('Catagory') }}
+            </x-nav-link>
+        </div>
+        <!-- cart -->
+        <div class=" space-x-8 sm:-py-px sm:pl-10 sm:flex">
          <x-nav-link :href="route('product.index')">
              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                  <path fill-rule="evenodd"
-                     d="M2.5 3A1.5 1.5 0 001 4.5v4A1.5 1.5 0 002.5 10h6A1.5 1.5 0 0010 8.5v-4A1.5 1.5 0 008.5 3h-6zm11 2A1.5 1.5 0 0012 6.5v7a1.5 1.5 0 001.5 1.5h4a1.5 1.5 0 001.5-1.5v-7A1.5 1.5 0 0017.5 5h-4zm-10 7A1.5 1.5 0 002 13.5v2A1.5 1.5 0 003.5 17h6a1.5 1.5 0 001.5-1.5v-2A1.5 1.5 0 009.5 12h-6z"
+                     d="M6 5v1H4.667a1.75 1.75 0 00-1.743 1.598l-.826 9.5A1.75 1.75 0 003.84 19H16.16a1.75 1.75 0 001.743-1.902l-.826-9.5A1.75 1.75 0 0015.333 6H14V5a4 4 0 00-8 0zm4-2.5A2.5 2.5 0 007.5 5v1h5V5A2.5 2.5 0 0010 2.5zM7.5 10a2.5 2.5 0 005 0V8.75a.75.75 0 011.5 0V10a4 4 0 01-8 0V8.75a.75.75 0 011.5 0V10z"
                      clip-rule="evenodd" />
              </svg>
-             {{ __('Catagory') }}
+
+             {{ __('Cart') }}
          </x-nav-link>
      </div>
-     <!-- catagory -->
-     <div class="hidden space-x-8 sm:-py-px sm:pl-10 sm:flex">
-         <x-nav-link :href="route('product.index')">
-             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                 <path fill-rule="evenodd"
-                     d="M2.5 3A1.5 1.5 0 001 4.5v4A1.5 1.5 0 002.5 10h6A1.5 1.5 0 0010 8.5v-4A1.5 1.5 0 008.5 3h-6zm11 2A1.5 1.5 0 0012 6.5v7a1.5 1.5 0 001.5 1.5h4a1.5 1.5 0 001.5-1.5v-7A1.5 1.5 0 0017.5 5h-4zm-10 7A1.5 1.5 0 002 13.5v2A1.5 1.5 0 003.5 17h6a1.5 1.5 0 001.5-1.5v-2A1.5 1.5 0 009.5 12h-6z"
-                     clip-rule="evenodd" />
-             </svg>
-             {{ __('catagory') }}
-         </x-nav-link>
-     </div>
-     <!-- catagory -->
-     <div class="hidden space-x-8 sm:-py-px sm:pl-10 sm:flex">
+     <!-- about -->
+     <div class=" space-x-8 sm:-py-px sm:pl-10 sm:flex">
          <x-nav-link :href="route('product.index')">
              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                  <path
